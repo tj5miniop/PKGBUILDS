a78f9054bd654759cb2a680784690c39451e6cbe
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2024-10-17 21:19:28 +0100

    Added base files
    Paru Successfully built, added it to repo

---
+++ Scripts/Packages/AUR_HELPER.sh
@@ -0,0 +1,18 @@
+#THESE SCRIPTS ARE CONFIGURED TO RUN ON MY SYSTEM - A few things need to be changed (probably just the directories and username) for this to work on yours
+#!/bin/bash
+
+#export $PKGNAME paru
+#export $URL https://aur.archlinux.org/paru.git
+#export $SCRIPTMAINTAINER tj5miniop
+
+
+cd tmp && cd build
+
+git clone https://aur.archlinux.org/paru && sleep 3
+
+cd paru 
+
+paru -Bi .
+echo "building $($PKGNAME)...."
+sleep 4 
+echo 'Done, just copy the pkg file into the repo when ready!'
